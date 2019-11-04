5c07c0d532d9d876654bd63102e7237afd9fe84b
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2019-11-04 19:43:58 +0900

    solved 4a, 1a, 71a, 158a in Rust

---
+++ src/bin/1a.rs
@@ -0,0 +1,36 @@
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+#[allow(dead_code)]
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+#[allow(dead_code)]
+fn yn(result: bool) {
+    if result {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+fn main() {
+    let v = read_vec::<u64>();
+    let n = v[0];
+    let m = v[1];
+    let a = v[2];
+
+    let n1 = if n % a == 0 { n / a } else { n / a + 1 };
+    let m1 = if m % a == 0 { m / a } else { m / a + 1 };
+    println!("{}", m1 * n1);
+}
